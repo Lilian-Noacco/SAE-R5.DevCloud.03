2aedd36b4db34126a76c36a98b7503df4ae69143
Dorian <107360989+Pi1lot@users.noreply.github.com>
2024-11-25 16:27:29 +0100

    Update map.js

--- public/map.js
+++ public/map.js
@@ -1,6 +1,12 @@
 // map.js
-import { getObstacles } from './client.js';
 
+import { getObstacles } from './client.js';
+// Position et rayon de la zone de sécurité
+
+//A définir par le serveur via un msg
+let safeZoneX = 100;
+let safeZoneY = 100;
+let safeZoneRadius = 5000;
   // Dessiner les obstacles sur la carte
   export function drawObstacles(ctx, cameraX, cameraY) {
     ctx.fillStyle = 'gray';  // Couleur des obstacles
@@ -13,4 +19,25 @@ import { getObstacles } from './client.js';
   }
   
 
+export function shrinkSafeZone() {
+  if (safeZoneRadius > 250) {
+    safeZoneRadius -= 1;
+    console.log("Shrinking");
+  }
+}
+
+export function isPlayerOutsideSafeZone(playerX, playerY) {
+  const distanceToCenter = Math.sqrt(
+    Math.pow(playerX - safeZoneX, 2) + Math.pow(playerY - safeZoneY, 2)
+  );
+  return distanceToCenter > safeZoneRadius;
+}
+
+export function drawSafeZone(ctx, cameraX, cameraY) {
+  ctx.beginPath();
+  ctx.arc(safeZoneX - cameraX, safeZoneY - cameraY, safeZoneRadius, 0, Math.PI * 2);
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 3;
+  ctx.stroke();
+}
   
